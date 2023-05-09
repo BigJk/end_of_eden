5f441ddefed9c2e0cf63275be9eedf21b7913571
Daniel Schmidt <info@daniel-schmidt.me>
2023-05-09 20:08:54 +0200

    Fix clipboard import.

--- clipboard/clipboard_windows.go
+++ clipboard/clipboard_windows.go
@@ -1,13 +1,13 @@
 package clipboard
 
-import "github.com/atotto/clipboard"
+import clip "golang.design/x/clipboard"
 
 func Init() {
-	if err := clipboard.Init(); err != nil {
+	if err := clip.Init(); err != nil {
 		panic(err)
 	}
 }
 
 func Set(data string) {
-	clipboard.WriteAll(data)
+	clip.WriteAll(data)
 }
