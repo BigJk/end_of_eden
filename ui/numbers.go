a4767d7b3bae73f0dd4ad2a8712754429d470e97
Daniel Schmidt <info@daniel-schmidt.me>
2023-05-16 22:32:15 +0200

    ASCII number builder.

--- ui/numbers.go
+++ ui/numbers.go
@@ -1,5 +1,12 @@
 package ui
 
+import (
+	"fmt"
+	"github.com/samber/lo"
+	"strconv"
+	"strings"
+)
+
 var Numbers = []string{
 	` ██████  
 ██  ████ 
@@ -51,4 +58,11 @@ var Numbers = []string{
  ██████ 
      ██ 
  █████`,
+}
+
+func GetNumber(number int) string {
+	return strings.Join(lo.Map([]rune(fmt.Sprint(number)), func(char rune, index int) string {
+		num, _ := strconv.Atoi(string(char))
+		return Numbers[num]
+	}), "")
 }
